b2c2b302ed9b29a72b1fab56ca791c5a4923085b
pbeuar <pbeuar@gmail.com>
2024-12-02 19:30:47 +0100

    day 2 complete

--- 02/quiz2.py
+++ 02/quiz2.py
@@ -4,14 +4,7 @@
 Created on Sun Dec  1 14:25:14 2024
 
 @author: pbeuar
-
-day2
-all increasing or all decreasing.
-1~3
-
-1- How many reports are safe?
 """
-
 
 reports = []
 
@@ -20,37 +13,46 @@ with open('input') as f:
         reports.append([int(x) for x in line.strip().split()])
 
 
-diffs = []
-
-for report in reports:
-    diffs.append([report[x]-report[x+1] for x in range(len(report)-1)])
-
-
-
 def gap(report):
     if 0 in report:
         return False
     report_plus = [abs(x) for x in report]
     return min(report_plus) >= 1 and max(report_plus) <= 3
 
+
 def all_increasing_or_decreasing(report):
     first = report[0] > 0
-    for diff in report[1:]:
-        if not (diff > 0) == first:
+    for dif in report[1:]:
+        if not (dif > 0) == first:
             return False
     return True
 
 
-answer1 = len([x for x in diffs if gap(x) and all_increasing_or_decreasing(x)])
+def check_report(report):
+    diff = [report[x]-report[x+1] for x in range(len(report)-1)]
+    return gap(diff) and all_increasing_or_decreasing(diff)
 
 
+good_reports = []
+ok_reports = []
 
+for report in reports:
+    if check_report(report):
+        good_reports.append(report)
 
+    else:
+        for i in range(len(report)):
+            temp_report = report.copy()
+            del temp_report[i]
+            if check_report(temp_report):
+                ok_reports.append([report, i])
+                break
+            
+answer1 = len(good_reports)
+# 624
 
-
-
-
-
+answer2 = answer1 + len(ok_reports)
+# 658
 
 
     
