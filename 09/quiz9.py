41f4abd18a3de73307908d542b35698ca6efbf82
pbeuar <pbeuar@gmail.com>
2024-12-10 14:33:02 +0100

    day 9 complete

--- 09/quiz9.py
+++ 09/quiz9.py
@@ -15,43 +15,56 @@ for i, cell in enumerate(disk_map):
 
 checksum1 = 0
 
-for i,b in enumerate(blocks):
-    if b == '.':
-        last = blocks.pop()
-        while last == '.':
-            last = blocks.pop()
-        blocks[i] = last
-    checksum1 += blocks[i] * i
-
-
-# checksum1
-# 6258319840548
-
-
-blocks1 = []
-
-ID = 0
-
-for i, cell in enumerate(disk_map):
-    idx = '.'
-    if (i+1)%2: 
-        idx = ID
-        ID += 1
-    if cell != '0':
-        blocks1.append([idx] * int(cell))
-
-checksum2 = 0
-
 # for i,b in enumerate(blocks):
 #     if b == '.':
 #         last = blocks.pop()
 #         while last == '.':
 #             last = blocks.pop()
 #         blocks[i] = last
-#     checksum2 += blocks[i] * i
+#     checksum1 += blocks[i] * i
 
 
-        
+# checksum1
+# 6258319840548
+
+
+checksum2 = 0
+
+def find_empty_space(pos, length):
+    for i,b in enumerate(blocks):
+        if b == '.':
+            if all([x=='.' for x in blocks[i:i+length]]):
+                if i < pos:
+                    return i
+    return False
+
+def get_bunch(id):
+    pos = blocks.index(id)
+    length = blocks.count(id)
+    return (pos, length)
+
+
+ID = blocks[-1]
+
+for i in range(ID, -1, -1):
+    pos, length = get_bunch(i)
+    if new_pos := find_empty_space(pos, length):
+        for p in range(length):
+            blocks[new_pos+p] = blocks[pos+p]
+            blocks[pos+p] = '.'
+    
+    
+
+for i,b in enumerate(blocks):
+    if b != '.':
+        checksum2 += blocks[i] * i
+
+
+# checksum2
+# 6286182965311
+
+
+
 
 
 
--- runner.py
+++ runner.py
@@ -1,136 +1,27 @@
 
-
-disk_map = '2333133121414131402'
-
-# =================================================
-
-# blocks = ''
-
-# ID = 0
-
-# for i, cell in enumerate(disk_map):
-#     idx = '.'
-#     if (i+1)%2: 
-#         idx = str(ID)
-#         ID += 1
-#     blocks += int(cell) * idx
-
-# 00...111...2...333.44.5555.6666.777.888899
-
-# =================================================
-
-blocks = []
-
-ID = 0
-
-for i, cell in enumerate(disk_map):
-    idx = '.'
-    if (i+1)%2: 
-        idx = ID
-        ID += 1
-    if cell != '0':
-        blocks += [idx] * int(cell)
-
-
-blocks1 = []
-
-ID = 0
-
-for i, cell in enumerate(disk_map):
-    idx = '.'
-    if (i+1)%2: 
-        idx = ID
-        ID += 1
-    if cell != '0':
-        blocks1.append([idx] * int(cell))
-
-
-
-checksum = 0
-
-# for i,b in enumerate(blocks):
-#     if b == '.':
-#         last = blocks.pop()
-#         while last == '.':
-#             last = blocks.pop()
-#         blocks[i] = last
-#     checksum += blocks[i] * i
-
-
-
-# 0099811188827773336446555566
-# =========================================
-
-
-
-# for r,rb in enumerate(reversed(blocks)):
-#     for i,b in enumerate(blocks):
-#         if rb[0] != '.' and b[0] == '.' and len(b) >= len(rb):
-#             temp = rb
-#             blocks.remove(rb)
-#             blocks[i] = temp
-#             # dif = len(b) - len(temp)
-#             # if dif > 0:
-#             #     blocks.insert(i+1, ['.']*dif)
-
-#             # blocks[-1-r] = len(rb)*'.'
-
-#             break
-            
-    
-# for r in range(len(blocks)-1, -1, -1):
-#     print(blocks[r])
-#     for i,b in enumerate(blocks):
-#         if b[0] == '.' and len(b) >= len(blocks[r]):
-#             blocks[i] = blocks.pop(r)
-#             dif = len(b) - len(blocks[1])
-#             if dif > 0:
-#                 print(dif)
-#                 blocks.insert(i+1, ['.'] * dif)
-#                 break
-                
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+data_map = """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+"""
+[[8, 9, 0, 1, 0, 1, 2, 3],
+ [7, 8, 1, 2, 1, 8, 7, 4],
+ [8, 7, 4, 3, 0, 9, 6, 5],
+ [9, 6, 5, 4, 9, 8, 7, 4],
+ [4, 5, 6, 7, 8, 9, 0, 3],
+ [3, 2, 0, 1, 9, 0, 1, 2],
+ [0, 1, 3, 2, 9, 8, 0, 1],
+ [1, 0, 4, 5, 6, 7, 3, 2]]
+"""
+
+data_map = [[int(x) for x in y] for y in data_map.splitlines()]
+
+
+# look for 0. mark as root. (look in all directions for 0 +1 add to zero)
+# when reaches 9 add coor to the 0 entry in trails dict
+# ? naybe need to keep end trails for less computations 
 
